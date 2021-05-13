169cb43eafc16fbc9a28dd1dfb474d4481ed74dd
Boomer#1111 <60472348+boomer1111@users.noreply.github.com>
2021-05-13 17:12:06 +0530

    h

--- config.lua
+++
@@ -1,8 +0,0 @@
-QBCore = nil
-
-TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-
-
-QBCore.Commands.Add("blipsbuilder", "blipsbuilder", {}, false, function(source, args)
-    TriggerClientEvent('bc:OpenMenu', source)
-end, "admin") 
