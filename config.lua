771457d908c6311e996a1433a94ecbec0648f975
Boomer#1111 <60472348+boomer1111@users.noreply.github.com>
2021-05-13 16:49:46 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 --Replace QBCore with Your Core Ex: GGCore
 QBCore = nil
 
-TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end))
+TriggerEvent('QBCore:GetObject', function(obj) XPFramework = obj end)
 
 
 QBCore.Commands.Add("blipsbuilder", "blipsbuilder", {}, false, function(source, args)
